4c62fc99ae165070d1140c08dea9793ea4efc90f
Kuba <kubik93@students.zcu.cz>
2016-10-04 10:12:39 +0200

    t

--- src/boot/main.cpp
+++ src/boot/main.cpp
@@ -4,6 +4,7 @@
 typedef void(__stdcall *TRun_VM)();
 
 int main() {
+
 	printf("boot test\n");
 	HMODULE kernel = LoadLibrary(L"kernel.dll");
 	TRun_VM vm = (TRun_VM)GetProcAddress(kernel, "Run_VM");
