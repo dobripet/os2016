98e8fde1eda8227c65dcda6d9c7dd5d801215053
Kuba <kubik93@students.zcu.cz>
2016-10-20 22:54:34 +0200

    Parsování příkazů. Výstup je vektor příkazů (=struktura Command_params, která uchovává info o jednom příkazu - kam přesměrování výstupu, jaké přepínače atd.). Nekontroluje ale syntax konkrétního příkazu - např. jestli jsou validní přepínače atd. - představuju si to tak, že si tohle bude muset každý příkaz kontrolovat sám.

---
+++ src/user/parser.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <set>
+#include <regex>
+
+#include "parser.hpp"
+
+const bool OK = true;
+const bool ERR = !OK;
+const std::set<std::string> SET_valid_commands = {"md", "wc", "rd", "echo", "dir", "type", "freq", "rgen", "ps", "sort"};  //+shell?
+const std::regex RGX_nab("[^a-zA-z]");
+const std::regex RGX_command_params("<|>|\"|/[a-zA-z]+|[^ \t\"]+");
+
+std::string Parser::get_error_message() {
+	return err_msg;
+}
+
+bool Parser::parse_commands(std::string line, std::vector<struct Command_params> * commands) {
+
+	std::vector<std::string> commandsStr;
+	size_t pos = line.find('\"'), oldpos = 0, lastpos = 0;
+	size_t posQbeg = 0, posQend = 0, posPipe = 0;
+	
+	// neni "  -- muzeme splitnout podle vsech | abychom ziskali jednotlive prikazy
+	if (pos == std::string::npos) {
+		while((pos = line.find('|', oldpos)) != std::string::npos) {
+			commandsStr.push_back(line.substr(oldpos, pos - oldpos));
+			oldpos = pos + 1;
+		}
+		commandsStr.push_back(line.substr(oldpos, std::string::npos));
+		
+	// je "  -- muzeme splitnout podle | jen pokud se nenachazi mezi lichym a sudym po sobe jdoucim "
+	} else {		
+		while(posPipe != std::string::npos) {
+		
+			posQbeg = line.find('\"', oldpos);
+			posQend = line.find('\"', posQbeg + 1);
+			posPipe = line.find('|', oldpos);
+			
+			//k oteviraci " chybi zaviraci
+			if (posQend == std::string::npos && posQbeg != std::string::npos) {
+				err_msg = "Wrong \" parity.";
+				return ERR;
+			}
+			// | je mezi ""
+			if (posPipe < posQend && posPipe > posQbeg) {
+				oldpos = posQend + 1;
+			}
+			else {
+				if (posPipe - lastpos <= 1) {
+					err_msg = "Multiple consecutive | symbols.";
+					return ERR;
+				}
+				commandsStr.push_back(line.substr(lastpos, posPipe - lastpos));
+				oldpos = posPipe + 1;
+				lastpos = oldpos;
+			}
+		}				
+	}
+	for (std::string com : commandsStr) {
+		Command_params par;
+		if (!parse_command(com, &par)) {
+			return false;
+		} else {
+			commands->push_back(par);
+		}
+	}
+	return OK;
+}
+
+bool Parser::parse_command(std::string command, struct Command_params * paramz) {
+	
+	*paramz = {"", false, false, false, "", "", ""};
+	bool leftpending = false, rightpending = false;
+	int pos = 0;
+	
+	//skip leading whitespace
+	while (isspace(command[pos]) != 0) {
+		pos++;
+	}
+	command = command.substr(pos);
+	
+	//separate command word
+	std::smatch command_match;
+	std::regex_search(command, command_match, RGX_nab);
+	std::string command_keyword = command.substr(0, command_match.position());
+	command = command.substr(command_match.position(), std::string::npos);
+	
+	//check whether command word is valid
+	if(SET_valid_commands.count(command_keyword) < 1) {
+		err_msg = "Unknown command \"" + command_keyword + "\"";	
+		return ERR;
+	}
+	paramz->com = command_keyword;
+	
+	while(std::regex_search(command, command_match, RGX_command_params)) {
+		
+		std::string str = command_match.str();
+		command = command_match.suffix();
+		
+		if (str == "\"") {
+			size_t len = command.find('"');
+			std::string par = command.substr(0, len);
+			if (leftpending) {
+				paramz->stdinpath = par;
+				leftpending = false;
+			} else if (rightpending) {
+				paramz->stdoutpath = par;
+				rightpending = false;
+			} else {
+				paramz->params.push_back(par);
+			}
+			command = command.substr(len + 1, std::string::npos);
+		}
+		else if (str == "<") {
+			if (paramz->redirectstdin) {
+				err_msg = "Attempting to redirect stdin twice.";
+				return ERR;
+			}		
+			paramz->redirectstdin = true;
+			if (leftpending || rightpending) {
+				err_msg = "Two redirect symbols in a row.";
+				return ERR;
+			}
+			leftpending = true;
+		}
+		else if (str == ">") {
+			if (paramz->redirectstdout) {
+				err_msg = "Attempting to redirect stdout twice.";
+				return ERR;
+			}		
+			paramz->redirectstdout = true;
+			if (rightpending || leftpending) {
+				err_msg = "Two redirect symbols in a row.";
+				return ERR;
+			}
+			rightpending = true;			
+		}
+		else if (str[0] == '/') {
+			if (leftpending || rightpending) {
+				err_msg = "Symbol / is followed by > or < symbol.";
+				return ERR;
+			}
+			if(str.length() == 1) {
+				err_msg = "Symbol / is not followed by alhpabetic symbols.";
+				return ERR;
+			} 
+			if (str[1] == '/') {
+				err_msg = "Symbol / is followed by another / symbol.";
+				return ERR;
+			}
+			paramz->hasswitches = true;
+			paramz->switches = paramz->switches + str.substr(1, std::string::npos);
+		}
+		else {
+			if (leftpending) {
+				paramz->stdinpath = str;
+				leftpending = false;
+			} else if (rightpending) {
+				paramz->stdoutpath = str;
+				rightpending = false;
+			} else {
+				paramz->params.push_back(str);
+			}
+		}		
+	}
+	
+	if (leftpending || rightpending) {
+		err_msg = "Command ends with redirect symbol.";
+		return ERR;
+	}
+	return OK;
+}
+
+
+
---
+++ src/user/parser.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <string>
+#include <vector>
+
+/*
+num Command {
+	rd,
+	md,
+	echo,
+	dir,
+	freq,
+	rgen,
+	ps,
+	type,
+	wc,
+	sort
+};
+*/
+
+typedef struct Command_params {
+
+//Command com;
+	std::string com;
+	bool redirectstdout;
+	bool redirectstdin;
+	bool hasswitches;
+	std::string switches;
+	std::string stdoutpath;
+	std::string stdinpath;
+	std::vector<std::string> params;	
+	
+} Command_params;
+
+typedef struct Parser {
+
+	private:
+		std::string err_msg;
+		bool parse_command(std::string line, struct Command_params * command);
+
+	public:
+		Parser() : err_msg("No error.") {};
+		std::string get_error_message();
+		bool parse_commands(std::string line, std::vector<struct Command_params> * commands);
+		//~Parser();
+		
+} Parser;
