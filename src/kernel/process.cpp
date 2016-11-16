51e49eae23735791526694481d6e0f7a841bcccc
Kuba <kubik93@students.zcu.cz>
2016-11-17 00:26:49 +0100

    proces

--- src/kernel/process.cpp
+++ src/kernel/process.cpp
@@ -8,13 +8,12 @@
 
 std::mutex process_table_mtx; //mutex for process table
 PCB * process_table[PROCESS_TABLE_SIZE] = { nullptr };//process table with max 1024 processes
-std::unordered_map< std::thread::id, int> TIDtoPID;
-
+std::unordered_map< std::thread::id, int> TIDtoPID; //mapovani std::thread::id na nas pid
 
 void HandleProcess(CONTEXT & regs) {
 	switch (Get_AL((__int16)regs.Rax)) {
+
 	case scCreateProcess: {
-		//std::thread t;
 		int pid = -1;
 		regs.Rax = (decltype(regs.Rax))createProcess((command_params *)regs.Rcx, &pid);
 		regs.Rdx = (decltype(regs.Rdx))pid;
@@ -46,7 +45,6 @@ void runProcess(TEntryPoint func, int pid, int argc, char ** argv, char * switch
 	regs.R11 = (decltype(regs.R11))process_table[pid]->IO_descriptors[3]; //current dir
 	regs.R12 = (decltype(regs.R12))switches;
 	regs.Rax = (decltype(regs.Rax))process_table[pid]->name;
-	//regs.Rbx = (decltype(regs.Rbx))pid; //?
 	regs.Rcx = (decltype(regs.Rcx))argc;
 	regs.Rdx = (decltype(regs.Rdx))argv;
 	regs.R13 = (decltype(regs.R13))&(process_table[pid]->currentPath);
@@ -57,11 +55,6 @@ void runProcess(TEntryPoint func, int pid, int argc, char ** argv, char * switch
 		int ret = close_file(handle);
 		//ret? nemusi se povest zavrit? 
 	}
-
-	//std::lock_guard<std::mutex> lock(process_table_mtx);
-	//TIDtoPID.erase(std::this_thread::get_id());
-//	delete process_table[pid];
-//	process_table[pid] = nullptr;
 }
 
 int createProcess(command_params * par, int *proc_pid)
@@ -84,14 +77,12 @@ int createProcess(command_params * par, int *proc_pid)
 		return -1;
 	}
 
-	//process_table[pid]->pid = pid;
 	for (auto &handle : par->handles) {
 		process_table[pid]->IO_descriptors.push_back(handle);
 	}
 	process_table[pid]->name = par->name;
-	//TODO getPathFromNode() misto node->name
-	//process_table[pid]->currentPath = opened_files_table[opened_files_table_instances[process_table[pid]->IO_descriptors[3]]->file]->node->name;
 	getPathFromNode(opened_files_table[opened_files_table_instances[process_table[pid]->IO_descriptors[3]]->file]->node, &(process_table[pid]->currentPath));
+
 	TEntryPoint func = (TEntryPoint)GetProcAddress(User_Programs, par->name);
 	if (!func) {
 		//vstupni bod se nepovedlo nalezt v uzivatelskych programech
@@ -102,23 +93,11 @@ int createProcess(command_params * par, int *proc_pid)
 		return -1;
 	}
 
-	//std::thread t(runProcess, func, pid, par->argc, par->argv, par->switches);
+	//konecne spustime proces
 	process_table[pid]->thr = std::thread(runProcess, func, pid, par->argc, par->argv, par->switches);
 	*proc_pid = pid;
-
-	//*t = std::thread(runProcess, func, pid, par->argc, par->argv, par->switches);
-
-	/*
-	if (par->waitForProcess) {
-		process_table[pid]->thr.join();
-	}
-	else {
-		process_table[pid]->thr.detach();
-	}
-	*/
 	return 0;
 }
-
 
 int joinProcess(int pid) {
 	std::thread::id tid = process_table[pid]->thr.get_id();
