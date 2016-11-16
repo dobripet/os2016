8b6de08ec217f0c933d868d8477cdedad88a0a0b
Petr <petr.dobricka@seznam.cz>
2016-11-16 08:26:37 +0100

    echo fix 0 params

--- src/user/c_echo.cpp
+++ src/user/c_echo.cpp
@@ -6,29 +6,41 @@
 size_t __stdcall echo(const CONTEXT &regs) {
 
 	FDHandle STDOUT = (FDHandle)regs.R9;
-
-	std::cout << "DEBUG:echo volano s poctem parametru: " << regs.Rcx << "\n";
-	for (int i = 0; i < (int)regs.Rcx; i++) {
-		std::cout << "DEBUG:echo param " << i << ": " << ((char**)regs.Rdx)[i] << "\n";
+	FDHandle STDERR= (FDHandle)regs.R10;
+	size_t total; 
+	size_t size;
+	/*No params*/
+	if ((int)regs.Rcx == 0) {
+		char * msg = "ECHO is on.\0";
+		size = strlen(msg);
+		bool success = Write_File(STDOUT, (char *)msg, size, &total);
 	}
-	std::string text = "";
-	for (int i = 0; i < (int)regs.Rcx; i++) {
-		text += (std::string)((char**)regs.Rdx)[i] + " ";
-	}
-	text.replace(text.length() - 1, 1, "\0");
-	size_t size = text.length();
-	size_t written;
-	std::cout << "DEBUG:echo text je " << text << "\n";
-	bool success = Write_File(STDOUT, (char *)text.c_str(), size, &written);
-	size_t total = written;
-	while (success && total != size) {
-		std::string rest = text.substr(total);
-		success = Write_File(STDOUT, (char *)rest.c_str(), rest.length(), &written);
-		total += written;
+	else {
+		size_t written;
+		std::cout << "DEBUG:echo volano s poctem parametru: " << regs.Rcx << "\n";
+		for (int i = 0; i < (int)regs.Rcx; i++) {
+			std::cout << "DEBUG:echo param " << i << ": " << ((char**)regs.Rdx)[i] << "\n";
+		}
+		std::string text = "";
+		for (int i = 0; i < (int)regs.Rcx; i++) {
+			text += (std::string)((char**)regs.Rdx)[i] + " ";
+		}
+		text.replace(text.length() - 1, 1, "\0");
+		size = text.length();
+		std::cout << "DEBUG:echo text je " << text << "\n";
+		bool success = Write_File(STDOUT, (char *)text.c_str(), size, &written);
+		total = written;
+		while (success && total != size) {
+			std::string rest = text.substr(total);
+			success = Write_File(STDOUT, (char *)rest.c_str(), rest.length(), &written);
+			total += written;
+		}
 	}
 	/*Handle not all has been written*/
 	if (total != size) {
 		std::cout << "DEBUG:echo error\n";
+		char * msg = "ECHO: error - not all data written(possibly closed file handle)\0";
+		Write_File(STDERR, (char *)msg, strlen(msg), &total);
 		return (size_t)1;
 	}
 	else {
