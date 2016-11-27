1fd68fea175ccc5e82d9b39cfef81912acfe025d
Kissa <Kissa@DESKTOP-PGMANV6>
2016-11-27 22:44:55 +0100

    fs komentare

--- src/kernel/filesystem.h
+++ src/kernel/filesystem.h
@@ -15,16 +15,32 @@ typedef struct node {
 	struct node *parent;
 } node;
 
+//vraci korenovy uzel filesystemu (C://)
 node *getRoot();
 
+//vytvori slozku a prida ji na pozadovanou adresu do adresarove struktury
 HRESULT mkdir(node **dir, char *path, node *currentDir);
-//HRESULT getNodeFromPath(char *path, node *currentDir, node **file);
+
+//vraci odkaz na uzel (soubor, nebo slozka) ze zadane adresy (relativni i absolutni) a aktualniho pracovniho adresare
 HRESULT getNodeFromPath(char *path, bool last, node *currentDir, node **node);
+
+//vraci absolutni cestu pro zadany (existujici) uzel v adresarove strukture
 HRESULT getPathFromNode(node *currentDir, std::string *path);
+
+//vraci odkaz na soubor, ktery chceme otevrit pro cteni/zapis (u slozky pro nastaveni aktualniho adresare nejakeho procesu)
 HRESULT openFile(node **file, char *path, bool rewrite, bool create, node *node);
+
+//pridava uzel do adresarove struktury
 HRESULT addChild(node **parent, struct node **child);
-//HRESULT deleteFile(std::string actualDirectory, std::string path);
+
+//smaze uzel z adresarove struktury
 HRESULT deleteNode(node *toDelete);
+
+//vraci pozadovana data ze zadaneho uzlu
 HRESULT getData(node **file, size_t startPosition, size_t size, char** buffer, size_t *filled);
+
+//zapise data do pozadovaneho souboru
 HRESULT setData(node **file, char* buffer, size_t write);
+
+//rozdeluje zadanou cestu na tokeny
 std::vector<std::string> split_string(std::string s);
