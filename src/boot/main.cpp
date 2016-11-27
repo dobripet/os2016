b5f874658a175de44267fd88a489f9ea5b525f94
Kuba <kubik93@students.zcu.cz>
2016-11-27 19:58:55 +0100

    boot

--- src/boot/main.cpp
+++ src/boot/main.cpp
@@ -1,9 +1,11 @@
 #include <Windows.h>
 
-/*takhle jsme kod mainu dostali, takze jsme to nememenili - nepokouseli se warningy odstranit, pouze je vypnuli */
-//C4191: 'type cast': unsafe conversion from 'FARPROC' to 'TRun_VM' 
-//C4191: 'main': must be '__cdecl'
+/* takhle jsme kod mainu dostali, takze jsme to nememenili - nepokouseli se warningy odstranit, pouze je vypnuli */
+// C4191 = 'type cast': unsafe conversion from 'FARPROC' to 'TRun_VM' 
+// C4007 = 'main': must be '__cdecl'
+#pragma warning ( push )
 #pragma warning( disable : 4191 4007 )
+
 
 typedef void(__stdcall *TRun_VM)();
 
@@ -15,3 +17,4 @@ int main() {
 	FreeLibrary(kernel);
 	return 0;
 }
+#pragma warning ( pop )
